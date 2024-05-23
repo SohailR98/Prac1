e556392465c28b33917872dafbfc3bfffe779e40
SohailR98 <sohailrr8@gmail.com>
2024-05-23 16:24:00 +0400

    linked_list

--- linked_list.py
+++ linked_list.py
@@ -10,6 +10,40 @@ class Linkedlist:
     def __init__(self):
         self.head = None
 
+    def push(self, new_data):
+
+        new_node = Node(new_data)
+
+        new_node.next = self.head
+
+        self.head = new_node
+
+    def deleteN(self, position):
+        if self.head is None:
+            return
+
+        temp = self.head
+        prev = self.head
+        
+        if position == 0:
+            self.head = temp.next
+            temp = None
+            return
+        
+        for i in range(position):
+            prev = temp
+            temp = temp.next
+
+            if temp is None:
+                break
+
+        if temp is not None:
+            prev.next = temp.next
+            temp = None
+
+
+
+
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -39,3 +73,16 @@ linked_list.append(3)
 
 
 linked_list.print_list()
+
+print("linked list after adding new elements")
+
+linked_list.push(4)
+linked_list.push(5)
+linked_list.push(6)
+linked_list.print_list()
+
+print("deletion of 3rd element")
+
+linked_list.deleteN(4)
+linked_list.print_list()
+
